f627f47adb3b781ce8dace3d6e7ed949a7b00ab2
Michael McGrath <mokeseven7@gmail.com>
2023-05-28 13:30:42 -0600

    Initial commit

---
+++ test/okta.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Okta from '../lib/okta-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/okta-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Okta.OktaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
